4f983c65e1c4c28a5fc112d2bb6520c4948a76b8
PantheraNestah <pantheranestah@gmail.com>
2024-06-27 07:06:09 +0300

    added clients and properties carousel

--- src/components/legend/Legend.js
+++ src/components/legend/Legend.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "./Legend.scss";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import Img1 from "../../assets/img/AI-gen-clients1.jpg";
+import Img2 from "../../assets/img/AI-gen-clients2.jpg";
+import Img3 from "../../assets/img/AI-gen-clients4.jpg";
+import Img4 from "../../assets/img/AI-gen-prop1.jpg";
+import Img5 from "../../assets/img/AI-gen-prop3.jpg";
+import Img6 from "../../assets/img/AI-gen-prop4.jpg";
+
 const Legend = () => {
     return (
         <section className="legend_section border">
@@ -9,8 +19,54 @@ const Legend = () => {
                 <div className="stats_highlight"></div>
             </div>
             <div className="highlight_slider col-lg-9 d-flex border">
-                <div className="slider"></div>
-                <div className="slider"></div>
+                <div id="client_carousel" className="slider carousel slide" data-bs-ride="carousel">
+                    <div class="slider_text d-flex flex-column align-items-left col-5">
+                        <span>Satisfied clients</span>
+                    </div>
+                    <div className="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src={Img1} alt="" className="d-block" />
+                        </div>
+                        <div class="carousel-item">
+                            <img src={Img2} alt="" className="d-block" />
+                        </div>
+                        <div class="carousel-item">
+                            <img src={Img3} alt="" className="d-block" />
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#client_carousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#client_carousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <div id="prop_carousel" className="slider carousel slide" data-bs-ride="carousel">
+                    <div class="slider_text d-flex flex-column align-items-left col-5">
+                        <span>Properties</span>
+                    </div>
+                    <div className="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src={Img4} alt="" className="d-block" />
+                        </div>
+                        <div class="carousel-item">
+                            <img src={Img5} alt="" className="d-block" />
+                        </div>
+                        <div class="carousel-item">
+                            <img src={Img6} alt="" className="d-block" />
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#prop_carousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#prop_carousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
             </div>
         </section>
     );
